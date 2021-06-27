316f36909a6115bd092a603877098884273af467
joaom <joao.monteiro95@hotmail.com>
2021-06-27 23:47:45 +0200

    Finished module 5 - CanActivate guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,22 @@ import { HeroesModule } from './heroes/heroes.module';
 import { CrisisCenterModule } from './crisis-center/crisis-center.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
+import { AdminModule } from './admin/admin.module';
+import { LoginComponent } from './auth/login/login.component';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    ComposeMessageComponent
+    ComposeMessageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    AuthModule,
+    AdminModule,
     FormsModule,
     BrowserAnimationsModule,
     CrisisCenterModule,
