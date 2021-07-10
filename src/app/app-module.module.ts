2e13e8b053c0d6332ab7c24e42ff34ab929374ff
joaom <joao.monteiro95@hotmail.com>
2021-07-10 17:15:37 +0200

    Module 6 - Custom pre-loading

--- src/app/app-module.module.ts
+++ src/app/app-module.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PreloadAllModules } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
 import { AuthGuard } from './auth/auth.guard';
+import { SelectivePreloadingStrategyService } from './selective-preloading-strategy.service';
 
 const appRoutes: Routes = [
   { 
@@ -13,7 +13,10 @@ const appRoutes: Routes = [
   },
   { 
     path: 'crisis-center',
-    loadChildren: () => import('./crisis-center/crisis-center.module').then(m => m.CrisisCenterModule)
+    loadChildren: () => import('./crisis-center/crisis-center.module').then(m => m.CrisisCenterModule),
+    data: {
+      preload: true
+    }
   },
   { path: 'compose', component: ComposeMessageComponent, outlet: 'popup' },
   { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
@@ -25,7 +28,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       {
-        preloadingStrategy: PreloadAllModules
+        preloadingStrategy: SelectivePreloadingStrategyService
       }
     )
   ],
